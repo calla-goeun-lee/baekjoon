d3aacb69591fec612f5f68464d28f8cf9f837855
Calla Lee <111102866+calla-goeun-lee@users.noreply.github.com>
2025-09-27 21:44:08 -0500

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string input1;
+    int num;
+    cin >> input1;
+    cin >> num;
+    
+    cout << input1[num - 1];
+    
+    return 0;
+}
