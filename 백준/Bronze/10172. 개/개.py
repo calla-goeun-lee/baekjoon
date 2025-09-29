ac326aa80970178af6a4f3bb9a63f711b1bb985c
Calla Lee <111102866+calla-goeun-lee@users.noreply.github.com>
2025-09-28 22:02:32 -0500

    [Bronze V] Title: 개, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.py
@@ -0,0 +1,5 @@
+print(r"|\_/|")
+print(r"|q p|   /}")
+print('( 0 )"""\\')     # using single quote so python doesn't get confused with the doubles
+print(r'|"^"`    |')    # back slash before inner double quote so python doesn't get confused
+print(r"||_/=\\__|")
